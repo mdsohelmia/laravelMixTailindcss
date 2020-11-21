f8013db05ed4c79ba9d1d63e14d8f2edec3858ec
Sohel Mia <sohelcse1999@gmail.com>
2020-11-21 11:18:46 +0600

    update Tailwindcss 2.0.1

--- public/js/app.js
+++ public/js/app.js
@@ -2037,8 +2037,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\laraMix\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\laraMix\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\Users\Sohel Mia\Desktop\laravelMixTailindcss\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Sohel Mia\Desktop\laravelMixTailindcss\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
